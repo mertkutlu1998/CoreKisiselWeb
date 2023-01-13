f3bd099f3919e258cc0653c3f11d1b7489e72323
Mert kutlu <mert1998.kutlu@gmail.com>
2023-01-13 13:29:31 +0300

    ....

--- BusinessLayer/Concrete/ToDoListManager.cs
+++ BusinessLayer/Concrete/ToDoListManager.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Abstract;
 using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,38 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class ToDoListManager:IToDoListService
+    public class ToDoListManager : IToDoListService
     {
-        
+        IToDoListDal _toDoList;
+
+        public ToDoListManager(IToDoListDal toDoList)
+        {
+            _toDoList = toDoList;
+        }
+
+        public void TAdd(ToDoList t)
+        {
+            _toDoList.Insert(t);
+        }
+
+        public void TDelete(ToDoList t)
+        {
+            _toDoList.Delete(t);
+        }
+
+        public ToDoList TGetByID(int id)
+        {
+            return _toDoList.GetByID(id);
+        }
+
+        public List<ToDoList> TGetList()
+        {
+            return _toDoList.GetList();
+        }
+
+        public void TUpdate(ToDoList t)
+        {
+            _toDoList.Update(t);
+        }
     }
 }
